c33d005a00589fc3010d70490ef544193b67c4a7
qthompson2 <q.thompson088@gmail.com>
2024-12-22 16:22:56 -0800

    Updates Screen to use a more modular `listener` system

--- data/computercraft/lua/rom/modules/main/display/screen.lua
+++ data/computercraft/lua/rom/modules/main/display/screen.lua
@@ -1,5 +1,7 @@
 ElementTypes = require("display.element_types")
 
+DEFAULT_LISTENERS = require("display.default_listeners")
+
 Screen = {}
 
 function Screen:new(elements, bg)
@@ -8,7 +10,7 @@ function Screen:new(elements, bg)
     obj.elements = elements or {}
     obj.bg = bg or colours.black
     obj.current_selection = nil
-    obj.data_functions = {}
+    obj.listeners = DEFAULT_LISTENERS
 
     obj.shift_held = false
 
@@ -102,215 +104,16 @@ function Screen:terminate()
     self.running = false
 end
 
-function Screen:terminateAll()
-    error("I don't know how to implement this rn")
-end
-
 function Screen:onEventRecieved(event_data)
     local event = event_data[1]
 
-    if event == "terminate" then
-        self:terminate()
-    elseif event == "key" then
-        local key = event_data[2]
-
-        if key == keys.tab then
-            if self.current_selection then
-                self.elements[self.current_selection]:clearSelection()
-            end
-
-            if self.current_selection == nil then
-                if self.shift_held then
-                    self.current_selection = #self.elements
-                else
-                    self.current_selection = 1
-                end
-            elseif self.current_selection >= #self.elements then
-                if self.shift_held then
-                    self.current_selection = self.current_selection - 1
-                else
-                    self.current_selection = 1
-                end
-            elseif self.current_selection <= 1 then
-                if self.shift_held then
-                    self.current_selection = #self.elements
-                else
-                    self.current_selection = self.current_selection + 1
-                end
-            elseif self.current_selection < #self.elements then
-                if self.shift_held then
-                    self.current_selection = self.current_selection - 1
-                else
-                    self.current_selection = self.current_selection + 1
-                end
-            end
-
-            if self.current_selection then
-                if self.elements[self.current_selection]:getType() == ElementTypes.COLUMN or self.elements[self.current_selection]:getType() == ElementTypes.ROW or self.elements[self.current_selection]:getType() == ElementTypes.BUNDLE then
-                    self.elements[self.current_selection]:getSelectedElement():setSelected(true)
-                elseif self.elements[self.current_selection]:getType() == ElementTypes.DROPDOWNMENU then
-                    self.elements[self.current_selection]:setOpen(true)
-                    self.elements[self.current_selection]:getSelectedElement():setSelected(true)
-                end
-            end
-        elseif key == keys.up then
-            local element = self.elements[self.current_selection]
-
-            if element then
-                if element:getType() == ElementTypes.COLUMN or element:getType() == ElementTypes.DROPDOWNMENU then
-                    element:getSelectedElement():setSelected(false)
-                    element:scroll(-1)
-                    element:getSelectedElement():setSelected(true)
-                end
-            end
-        elseif key == keys.down then
-            local element = self.elements[self.current_selection]
-
-            if element then
-                if element:getType() == ElementTypes.COLUMN or element:getType() == ElementTypes.DROPDOWNMENU then
-                    element:getSelectedElement():setSelected(false)
-                    element:scroll(1)
-                    element:getSelectedElement():setSelected(true)
-                end
-            end
-        elseif key == keys.enter then
-            local element = self.elements[self.current_selection]
-
-            if element then
-                if element:getType() == ElementTypes.BUTTON then
-                    element:getAction()()
-                elseif element:getType() == ElementTypes.COLUMN or element:getType() == ElementTypes.ROW or element:getType() == ElementTypes.DROPDOWNMENU or element:getType() == ElementTypes.BUNDLE then
-                    if element:getSelectedElement():getType() == ElementTypes.BUTTON then
-                        element:getSelectedElement():getAction()()
-                    end
-                end
-            end
-        elseif key == keys.backspace then
-            local element = self.elements[self.current_selection]
-
-            if element then
-                if element:getType() == ElementTypes.TEXTFIELD then
-                    element:backspace()
-                elseif element:getType() == ElementTypes.BUNDLE then
-                    if element:getSelectedElement():getType() == ElementTypes.TEXTFIELD then
-                        element:getSelectedElement():backspace()
-                    end
-                end
-            end
-        elseif key == keys.left then
-            local element = self.elements[self.current_selection]
-
-            if element then
-                if element:getType() == ElementTypes.TEXTFIELD then
-                    element:scroll(-1)
-                elseif element:getType() == ElementTypes.ROW or element:getType() == ElementTypes.BUNDLE then
-                    element:getSelectedElement():setSelected(false)
-                    element:scroll(-1)
-                    element:getSelectedElement():setSelected(true)
-                end
-            end
-        elseif key == keys.right then
-            local element = self.elements[self.current_selection]
-
-            if element then
-                if element:getType() == ElementTypes.TEXTFIELD then
-                    element:scroll(1)
-                elseif element:getType() == ElementTypes.ROW or element:getType() == ElementTypes.BUNDLE then
-                    element:getSelectedElement():setSelected(false)
-                    element:scroll(1)
-                    element:getSelectedElement():setSelected(true)
-                end
-            end
-        elseif key == keys.leftShift or key == keys.rightShift then
-            self.shift_held = true
-        end
-    elseif event == "char" then
-        local char = event_data[2]
-        local element = self.elements[self.current_selection]
-
-        if element then
-            if element:getType() == ElementTypes.TEXTFIELD then
-                element:appendInput(char)
-            elseif element:getType() == ElementTypes.BUNDLE then
-                if element:getSelectedElement():getType() == ElementTypes.TEXTFIELD then
-                    element:getSelectedElement():appendInput(char)
-                end
-            end
-        end
-    elseif event == "mouse_click" then
-        local button, x, y = event_data[2], event_data[3], event_data[4]
-        local element, sub_element = self:getElementAt(x, y)
-
-        self:clearSelection()
-
-        if element then
-            element:setSelected(true)
-            self.current_selection = self:getIndex(element)
-        end
-        if sub_element then
-            sub_element:setSelected(true)
-            self.current_selection = self:getIndex(element)
-        end
-
-        if element and button == 1 then
-            if element:getType() == ElementTypes.BUTTON then
-                element:getAction()()
-            elseif sub_element then
-                if sub_element:getType() == ElementTypes.BUTTON then
-                    sub_element:getAction()()
-                end
-            end
-        end
-
-        if element and button == 2 then
-            if element:getType() == ElementTypes.DROPDOWNMENU then
-                element:setOpen(not element:getOpen())
-            elseif sub_element then
-                if sub_element:getType() == ElementTypes.DROPDOWNMENU then
-                    sub_element:setOpen(not sub_element:getOpen())
-                end
-            end
-        end
-
-    elseif event == "mouse_scroll" then
-        local dir, x, y = event_data[2], event_data[3], event_data[4]
-
-        local container_element, _ = self:getElementAt(x, y)
-
-        self:clearSelection()
-
-        if container_element then
-            if container_element:getType() == ElementTypes.COLUMN or container_element:getType() == ElementTypes.ROW then
-                container_element:scroll(dir, true)
-            end
-        end
-    elseif event == "key_up" then
-        local key = event_data[2]
-
-        if key == keys.leftShift or key == keys.rightShift then
-            self.shift_held = false
-        end
-    elseif event == "modem_message" then
-        if self.onModemMessageReceived then
-            self.onModemMessageReceived(event_data)
-        end
+    if self.listeners[event] then
+        self.listeners[event](self, event_data)
     end
 end
 
-function Screen:handleData()
-    for _, data_function in ipairs(self.data_functions) do
-        data_function(self)
-    end
-end
-
-function Screen:addDataFunction(data_function)
-    table.insert(self.data_functions, data_function)
-end
-
-function Screen:addListener(event, listener) --Change this to be better
-    if event == "modem_message" then
-        self.onModemMessageReceived = listener
-    end
+function Screen:addListener(event, listener)
+    self.listeners[event] = listener
 end
 
 function Screen:removeDataFunction(data_function)
@@ -332,15 +135,10 @@ function Screen:run()
     while self.running do
         ---@diagnostic disable-next-line: undefined-field
         local event_data = {os.pullEventRaw()}
-        local event = event_data[1]
 
-        if event ~= "timer" then
-            self:onEventRecieved(event_data)
+        self:onEventRecieved(event_data)
+        if self.running then
             self:draw()
-        elseif event == "timer" then
-            self:handleData()
-            ---@diagnostic disable-next-line: undefined-field
-            os.startTimer(1)
         end
     end
 end
